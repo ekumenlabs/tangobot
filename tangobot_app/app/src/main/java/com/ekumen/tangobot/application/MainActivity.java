8d0793ac2661176de8dd3b5c81f638f63908860f
Juan Ignacio Ubeira <juan.iu@hotmail.com>
2017-03-16 13:21:15 -0300

    Separating Settings activity in two layers. -Toolbar working -Menu display added with no actions

--- tangobot_app/app/src/main/java/com/ekumen/tangobot/application/MainActivity.java
+++ tangobot_app/app/src/main/java/com/ekumen/tangobot/application/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Ekumen
+ * Copyright 2017 Ekumen, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,10 @@ import android.content.ServiceConnection;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 import com.ekumen.tangobot.loaders.KobukiNodeLoader;
@@ -36,7 +39,7 @@ import com.ekumen.tangobot.nodes.ParameterLoaderNode;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.ros.android.RosActivity;
+import org.ros.android.AppCompatRosActivity;
 import org.ros.node.ConnectedNode;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
@@ -52,7 +55,7 @@ import eu.intermodalics.tango_ros_node.TangoInitializationHelper;
 import eu.intermodalics.tango_ros_node.TangoInitializationHelper.DefaultTangoServiceConnection.AfterConnectionCallback;
 import eu.intermodalics.tango_ros_node.TangoRosNode;
 
-public class MainActivity extends RosActivity implements TangoRosNode.CallbackListener {
+public class MainActivity extends AppCompatRosActivity implements TangoRosNode.CallbackListener {
     private static final String ACTION_USB_PERMISSION = "com.github.rosjava.android.androidp1.USB_PERMISSION";
     public final static String APP_NAME = "TangoBotApp";
 
@@ -116,6 +119,8 @@ public class MainActivity extends RosActivity implements TangoRosNode.CallbackLi
 
         // UI
         setContentView(R.layout.main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // USB handling code
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
@@ -138,6 +143,13 @@ public class MainActivity extends RosActivity implements TangoRosNode.CallbackLi
                 onDeviceDetached(device);
             }
         };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     @Override
---
+++ tangobot_app/app/src/main/java/com/ekumen/tangobot/application/MasterChooserSettingsActivity.java
@@ -0,0 +1,153 @@
+package com.ekumen.tangobot.application;
+
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.Toolbar;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public abstract class MasterChooserSettingsActivity extends AppCompatPreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener{
+
+    protected SettingsPreferenceFragment mSettingsPreferenceFragment;
+    protected Log mLog;
+    protected SharedPreferences mSharedPref;
+
+    /**
+     * A preference value change listener that updates the preference's summary
+     * to reflect its new value.
+     */
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+
+            } else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    /**
+     * Binds a preference's summary to its value. More specifically, when the
+     * preference's value is changed, its summary (line of text below the
+     * preference title) is updated to reflect the value. The summary is also
+     * immediately updated upon calling this method. The exact display format is
+     * dependent on the type of preference.
+     *
+     * @see #sBindPreferenceSummaryToValueListener
+     */
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        mSharedPref.registerOnSharedPreferenceChangeListener(this);
+        setupActionBar();
+        mSettingsPreferenceFragment = new SettingsPreferenceFragment();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, mSettingsPreferenceFragment)
+                .commit();
+        mLog = LogFactory.getLog(MasterChooserSettingsActivity.class);
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    /**
+     * This method stops fragment injection in malicious applications.
+     * Make sure to deny any unknown fragments here.
+     */
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || SettingsPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    /**
+     * Fragment showing settings preferences.
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static class SettingsPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_settings);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference(getResources().getString(R.string.pref_master_uri_key)));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        // This result and this extra is expected by the standard ROS Activity
+        String uri = mSharedPref.getString(getResources().getString(R.string.pref_master_uri_key), "");
+        intent.putExtra("ROS_MASTER_URI", uri);
+        setResult(RESULT_OK, intent);
+        super.onBackPressed();
+    }
+}
